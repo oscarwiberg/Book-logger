92d9d3ee683a45e9c502a3614ef473e6e029a736
oscarwiberg <oscar.wiberg@live.se>
2020-07-08 16:41:19 +0200

    books routes done

--- mern-proj/backend/routes/books.js
+++ mern-proj/backend/routes/books.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-let Book = require('../models/book.model');
+const Book = require('../models/book.model');
 
 router.route('/').get((req, res) => {
   Book.find()
@@ -17,6 +17,34 @@ router.route('/add').post((req, res) => {
   newBook
     .save()
     .then(() => res.json('Book added :)'))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.route('/:id').get((req, res) => {
+  Book.findById(req.params.id)
+    .then((book) => res.json(book))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Book.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Book deleted :)'))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Book.findById(req.params.id)
+    .then((book) => {
+      book.username = req.body.username;
+      book.title = req.body.title;
+      book.pages = Number(req.body.pages);
+      book.date = Date.parse(req.body.date);
+
+      book
+        .save()
+        .then(() => res.json('Book saved :)'))
+        .catch((err) => res.status(400).json(err));
+    })
     .catch((err) => res.status(400).json(err));
 });
 
