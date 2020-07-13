32949af004a2394406fe752acb9ae768d2f7bfdb
oscarwiberg <oscar.wiberg@live.se>
2020-07-13 14:56:45 +0200

    BookList component and EditBook component done

--- mern-proj/frontend/src/components/BooksList.jsx
+++ mern-proj/frontend/src/components/BooksList.jsx
@@ -1,10 +1,82 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Book = (props) => (
+  <tr>
+    <td>{props.book.username}</td>
+    <td>{props.book.title}</td>
+    <td>{props.book.pages}</td>
+    <td>{props.book.date.substring(0, 10)}</td>
+    <td>
+      <Link to={'/edit/' + props.book._id}>edit</Link> |{' '}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteBook(props.book._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
 
 export default class BooksList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteBook = this.deleteBook.bind(this);
+
+    this.state = { books: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/books/')
+      .then((res) => {
+        this.setState({ books: res.data });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  deleteBook(id) {
+    axios
+      .delete('http://localhost:5000/books/' + id)
+      .then((res) => console.log(res.data));
+    this.setState({
+      books: this.state.books.filter((element) => element._id !== id),
+    });
+  }
+
+  booksList() {
+    return this.state.books.map((currentBook) => {
+      return (
+        <Book
+          book={currentBook}
+          deleteBook={this.deleteBook}
+          key={currentBook._id}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>Books list component</p>
+        <h3>Logged Books</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.booksList()}</tbody>
+        </table>
       </div>
     );
   }
--- mern-proj/frontend/src/components/EditBook.jsx
+++ mern-proj/frontend/src/components/EditBook.jsx
@@ -1,10 +1,145 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios';
 
 export default class EditBook extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangePages = this.onChangePages.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      title: '',
+      pages: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/books/' + this.props.match.params.id)
+      .then((res) => {
+        this.setState({
+          username: res.data.username,
+          title: res.data.title,
+          pages: res.data.pages,
+          date: new Date(res.data.date),
+        });
+      });
+
+    axios.get('http://localhost:5000/users/').then((res) => {
+      if (res.data.length > 0) {
+        this.setState({
+          users: res.data.map((user) => user.username),
+        });
+      }
+    });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+
+  onChangePages(e) {
+    this.setState({
+      pages: e.target.value,
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const book = {
+      username: this.state.username,
+      title: this.state.title,
+      pages: this.state.pages,
+      date: this.state.date,
+    };
+
+    axios.post(
+      'http://localhost:5000/books/update/' + this.props.match.params.id,
+      book
+    );
+
+    window.location = '/';
+  }
+
   render() {
     return (
       <div>
-        <p>Edit book component</p>
+        <h3>Edit book</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map((user) => {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Title: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.title}
+              onChange={this.onChangeTitle}
+            />
+          </div>
+          <div className="form-group">
+            <label>Total pages: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.pages}
+              onChange={this.onChangePages}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input type="submit" value="Edit book" className="btn btn-light" />
+          </div>
+        </form>
       </div>
     );
   }
